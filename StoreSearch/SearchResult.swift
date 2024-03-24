da65b5cef1554da1dacf82dcbb7c046b7745dd98
Benjamin Jaramillo <200308335@itcelaya.edu.mx>
2024-03-24 16:54:35 -0600

    cap 34

--- StoreSearch/SearchResult.swift
+++ StoreSearch/SearchResult.swift
@@ -4,9 +4,133 @@
 //
 //  Created by Benjamin Jaramillo on 03/02/24.
 //
-// Definición de la clase SearchResult
-class SearchResult {
-    // Propiedades de la clase
-    var name = ""         // Nombre del resultado de búsqueda
-    var artistName = ""   // Nombre del artista asociado al resultado de búsqueda
+
+
+import Foundation
+
+// Clase que representa un objeto que contiene un array de SearchResult
+class ResultArray: Codable {
+    // Propiedad que indica el número de resultados en el array
+    var resultCount = 0
+    // Propiedad que almacena los resultados de la búsqueda
+    var results = [SearchResult]()
+}
+
+
+// Sobrecarga del operador "<" para comparar dos objetos SearchResult
+func < (lhs: SearchResult, rhs: SearchResult) -> Bool {
+    // Compara los nombres de los dos objetos SearchResult utilizando el método localizedStandardCompare
+    return lhs.name.localizedStandardCompare(rhs.name) == .orderedAscending
+}
+
+
+class SearchResult: Codable, CustomStringConvertible {
+  var artistName: String? = ""
+  var trackName: String? = ""
+  var kind: String? = ""
+  var trackPrice: Double? = 0.0
+  var currency = ""
+  var imageSmall = ""
+  var imageLarge = ""
+  var trackViewUrl: String?
+  var collectionName: String?
+  var collectionViewUrl: String?
+  var collectionPrice: Double?
+  var itemPrice: Double?
+  var itemGenre: String?
+  var bookGenre: [String]?
+
+    // Enumeración que define las claves de codificación para decodificar los datos JSON
+    enum CodingKeys: String, CodingKey {
+        // Clave para el enlace de la imagen pequeña
+        case imageSmall = "artworkUrl60"
+        // Clave para el enlace de la imagen grande
+        case imageLarge = "artworkUrl100"
+        // Clave para el género del artículo
+        case itemGenre = "primaryGenreName"
+        // Clave para el género del libro
+        case bookGenre = "genres"
+        // Clave para el precio del artículo
+        case itemPrice = "price"
+        // Claves comunes para todos los tipos de elementos
+        case kind, artistName, currency
+        // Claves específicas para diferentes tipos de elementos
+        case trackName, trackPrice, trackViewUrl // Para canciones
+        case collectionName, collectionViewUrl, collectionPrice // Para álbumes o libros
+    }
+
+
+    // Propiedad computada que devuelve el nombre del elemento
+    var name: String {
+        // Utiliza el operador de fusión nula para devolver el primer valor no nulo entre trackName y collectionName
+        // Si ambos son nulos, devuelve una cadena vacía
+        return trackName ?? collectionName ?? ""
+    }
+
+    // Propiedad computada que devuelve la URL de la tienda del elemento
+    var storeURL: String {
+        // Utiliza el operador de fusión nula para devolver el primer valor no nulo entre trackViewUrl y collectionViewUrl
+        // Si ambos son nulos, devuelve una cadena vacía
+        return trackViewUrl ?? collectionViewUrl ?? ""
+    }
+
+    // Propiedad computada que devuelve el precio del elemento
+    var price: Double {
+        // Utiliza el operador de fusión nula para devolver el primer valor no nulo entre trackPrice, collectionPrice y itemPrice
+        // Si todos son nulos, devuelve 0.0 como precio predeterminado
+        return trackPrice ?? collectionPrice ?? itemPrice ?? 0.0
+    }
+
+    // Propiedad computada que devuelve el género del elemento
+    var genre: String {
+        // Comprueba si itemGenre tiene un valor
+        if let genre = itemGenre {
+            // Si itemGenre tiene un valor, lo devuelve
+            return genre
+        } else if let genres = bookGenre {
+            // Si itemGenre es nulo pero bookGenre tiene un valor, une todos los géneros con una coma y espacio y lo devuelve
+            return genres.joined(separator: ", ")
+        }
+        // Si tanto itemGenre como bookGenre son nulos, devuelve una cadena vacía
+        return ""
+    }
+
+
+    // Propiedad computada que devuelve el tipo de elemento
+    var type: String {
+        // Obtiene el valor de kind o utiliza "audiobook" como valor predeterminado si kind es nulo
+        let kind = self.kind ?? "audiobook"
+        
+        // Utiliza un switch para asignar un tipo basado en el valor de kind
+        switch kind {
+        case "album": return "Album"
+        case "audiobook": return "Audio Book"
+        case "book": return "Book"
+        case "ebook": return "E-Book"
+        case "feature-movie": return "Movie"
+        case "music-video": return "Music Video"
+        case "podcast": return "Podcast"
+        case "software": return "App"
+        case "song": return "Song"
+        case "tv-episode": return "TV Episode"
+        default: break
+        }
+        
+        // Si el valor de kind no coincide con ninguno de los casos anteriores, devuelve "Unknown"
+        return "Unknown"
+    }
+
+    // Propiedad computada que devuelve el nombre del artista
+    var artist: String {
+        // Utiliza el operador de fusión nula para obtener el valor de artistName o una cadena vacía si artistName es nulo
+        return artistName ?? ""
+    }
+
+  
+    // Propiedad computada que devuelve una descripción del resultado
+    var description: String {
+        // Utiliza interpolación de cadenas para construir la descripción con los valores de kind, name y artistName
+        return "\nResult - Kind: \(kind ?? "None"), Name: \(name), Artist Name: \(artistName ?? "None")"
+    }
+
 }
--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -15,62 +15,81 @@ class SearchViewController: UIViewController {
     var searchResults = [SearchResult]()
     var hasSearched = false
     
+    // Definición de la estructura TableView
     struct TableView {
-      struct CellIdentifiers {
-        static let searchResultCell = "SearchResultCell"
-        static let nothingFoundCell = "NothingFoundCell"
-      }
+        // Anidamiento de la estructura CellIdentifiers
+        struct CellIdentifiers {
+            // Definición de identificadores de celda estáticos para SearchResultCell y NothingFoundCell
+            static let searchResultCell = "SearchResultCell"  // Identificador de celda para los resultados de búsqueda
+            static let nothingFoundCell = "NothingFoundCell"  // Identificador de celda para mostrar cuando no se encuentran resultados
+        }
     }
-    
+
+    // Configuración inicial cuando la vista carga
     override func viewDidLoad() {
-      super.viewDidLoad()
-      tableView.contentInset = UIEdgeInsets(top: 47, left: 0, bottom: 0, right: 0)
-      var cellNib = UINib(nibName: TableView.CellIdentifiers.searchResultCell, bundle: nil)
-      tableView.register(cellNib, forCellReuseIdentifier: TableView.CellIdentifiers.searchResultCell)
-      cellNib = UINib(nibName: TableView.CellIdentifiers.nothingFoundCell, bundle: nil)
-      tableView.register(
-        cellNib,
-        forCellReuseIdentifier: TableView.CellIdentifiers.nothingFoundCell)
-      searchBar.becomeFirstResponder()
+        super.viewDidLoad()
+        
+        // Ajusta la inserción de contenido de la tabla para dejar espacio para la barra de búsqueda
+        tableView.contentInset = UIEdgeInsets(top: 47, left: 0, bottom: 0, right: 0)
+        
+        // Registra el archivo de Nib (Interface Builder) para la celda SearchResultCell
+        var cellNib = UINib(nibName: TableView.CellIdentifiers.searchResultCell, bundle: nil)
+        tableView.register(cellNib, forCellReuseIdentifier: TableView.CellIdentifiers.searchResultCell)
+        
+        // Registra el archivo de Nib para la celda NothingFoundCell
+        cellNib = UINib(nibName: TableView.CellIdentifiers.nothingFoundCell, bundle: nil)
+        tableView.register(cellNib, forCellReuseIdentifier: TableView.CellIdentifiers.nothingFoundCell)
+        
+        // Hace que el searchBar tenga el foco para que el teclado se muestre automáticamente
+        searchBar.becomeFirstResponder()
     }
+
 }
 
 // MARK: - Search Bar Delegate
-// Extensión del SearchViewController que adopta el protocolo UISearchBarDelegate
 extension SearchViewController: UISearchBarDelegate {
     
-    // Función llamada cuando se presiona el botón de búsqueda en el teclado
+    // Método llamado cuando se hace clic en el botón de búsqueda en la barra de búsqueda
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        // Renuncia al primer respondedor, es decir, oculta el teclado
-        searchBar.resignFirstResponder()
-        
-        // Limpia los resultados de búsqueda anteriores
-        searchResults = []
-        
-        // Verifica si el texto de búsqueda no es "justin bieber"
-        if searchBar.text! != "justin bieber" {
-            // Genera resultados de búsqueda falsos
-            for i in 0...2 {
-                let searchResult = SearchResult()
-                searchResult.name = String(format: "Fake Result %d for", i)
-                searchResult.artistName = searchBar.text!
-                searchResults.append(searchResult)
+        // Verifica si el texto de búsqueda no está vacío
+        if !searchBar.text!.isEmpty {
+            // Si hay texto de búsqueda, se resigna el primer respondedor (se oculta el teclado)
+            searchBar.resignFirstResponder()
+
+            // Se establece la bandera hasSearched en true para indicar que se ha realizado una búsqueda
+            hasSearched = true
+            
+            // Se limpia el array de resultados de búsqueda para realizar una nueva búsqueda
+            searchResults = []
+
+            // Se construye la URL para la búsqueda utilizando el texto de búsqueda ingresado en la barra de búsqueda
+            let url = iTunesURL(searchText: searchBar.text!)
+            // Se imprime la URL construida para depuración
+            print("URL: '\(url)'")
+
+            // Se realiza la solicitud al servidor de iTunes con la URL construida y se obtiene la respuesta de datos
+            if let data = performStoreRequest(with: url) {
+                // Si se obtienen datos de la solicitud, se parsean para obtener los resultados de búsqueda
+                searchResults = parse(data: data)
+                // Se ordenan los resultados de búsqueda en orden alfabético
+                searchResults.sort(by: <)
             }
+            
+            // Se recarga la tabla para mostrar los nuevos resultados de búsqueda
+            tableView.reloadData()
         }
-        
-        // Indica que se ha realizado una búsqueda
-        hasSearched = true
-        
-        // Recarga los datos de la tabla para mostrar los nuevos resultados de búsqueda
-        tableView.reloadData()
     }
+
     
-    // Función que especifica la posición de la barra de búsqueda
+    
+    // Devuelve la posición de la barra en la interfaz de usuario
     func position(for bar: UIBarPositioning) -> UIBarPosition {
-        // Devuelve la posición de la barra de búsqueda como 'topAttached'
+        // Devuelve la posición de la barra como "topAttached" para adjuntarla en la parte superior
         return .topAttached
     }
+
 }
+
 
 
 // MARK: - Table View Delegate
@@ -94,33 +113,49 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
         }
     }
     
+    
     // Función que configura y devuelve una celda para una fila específica de la tabla
+    // Método que configura y devuelve una celda para una fila específica de la tabla
     func tableView(
         _ tableView: UITableView,
         cellForRowAt indexPath: IndexPath
     ) -> UITableViewCell {
         // Verifica si no hay resultados de búsqueda
         if searchResults.count == 0 {
-            // Si no hay resultados, devuelve una celda de "Nothing Found" reutilizable
+            // Si no hay resultados, devuelve una celda de "Nada encontrado"
             return tableView.dequeueReusableCell(
                 withIdentifier: TableView.CellIdentifiers.nothingFoundCell,
                 for: indexPath)
         } else {
             // Si hay resultados de búsqueda
-            // Obtiene una celda reutilizable de tipo SearchResultCell
+            // Obtiene una celda reutilizable para SearchResultCell y la configura
             let cell = tableView.dequeueReusableCell(
                 withIdentifier: TableView.CellIdentifiers.searchResultCell,
                 for: indexPath) as! SearchResultCell
-            // Obtiene el objeto SearchResult correspondiente a la fila actual
+            // Obtiene el resultado de búsqueda correspondiente a la fila actual
             let searchResult = searchResults[indexPath.row]
-            // Configura las etiquetas de la celda con los datos del SearchResult
+            // Configura el nombre del resultado de búsqueda en la etiqueta nameLabel de la celda
             cell.nameLabel.text = searchResult.name
-            cell.artistNameLabel.text = searchResult.artistName
+            // Verifica si el artista está vacío
+            if searchResult.artist.isEmpty {
+                // Si el artista está vacío, muestra "Desconocido" en la etiqueta artistNameLabel
+                cell.artistNameLabel.text = "Unknown"
+            } else {
+                // Si el artista no está vacío, muestra el nombre del artista y el tipo de resultado en artistNameLabel
+                cell.artistNameLabel.text = String(
+                    format: "%@ (%@)",
+                    searchResult.artist,
+                    searchResult.type)
+            }
             // Devuelve la celda configurada
             return cell
         }
     }
 
+    
+    
+    
+    
     // Función que se ejecuta cuando se selecciona una fila de la tabla
     func tableView(
         _ tableView: UITableView,
@@ -143,4 +178,72 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
             return indexPath
         }
     }
+    
+    
+    
+    
+    // MARK: - Helper Methods
+    // Método para construir y devolver la URL de búsqueda en iTunes
+    func iTunesURL(searchText: String) -> URL {
+        // Codifica el texto de búsqueda para asegurarse de que sea compatible con una URL
+        let encodedText = searchText.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)!
+        // Construye la URL de búsqueda en iTunes utilizando el texto de búsqueda codificado
+        let urlString = String(format: "https://itunes.apple.com/search?term=%@", encodedText)
+        // Crea una instancia de URL a partir de la cadena de URL construida
+        let url = URL(string: urlString)
+        // Devuelve la URL creada
+        return url!
+    }
+
+    
+    // Método para realizar una solicitud al servidor de iTunes y devolver los datos obtenidos
+    func performStoreRequest(with url: URL) -> Data? {
+        do {
+            // Intenta obtener los datos de la URL proporcionada
+            return try Data(contentsOf: url)
+        } catch {
+            // Si se produce un error al obtener los datos, imprime el error y muestra un mensaje de error en la red
+            print("Download Error: \(error.localizedDescription)")
+            showNetworkError()
+            // Devuelve nil indicando que no se pudieron obtener los datos
+            return nil
+        }
+    }
+
+    
+    // Método para analizar los datos JSON obtenidos de la solicitud y devolver un array de SearchResult
+    func parse(data: Data) -> [SearchResult] {
+        do {
+            // Crea una instancia de JSONDecoder para decodificar los datos JSON
+            let decoder = JSONDecoder()
+            // Decodifica los datos JSON en un objeto de tipo ResultArray utilizando el JSONDecoder
+            let result = try decoder.decode(ResultArray.self, from: data)
+            // Devuelve la matriz de resultados obtenidos del objeto ResultArray
+            return result.results
+        } catch {
+            // Si se produce un error al decodificar los datos JSON, imprime el error y devuelve un array vacío
+            print("JSON Error: \(error)")
+            return []
+        }
+    }
+
+    // Método para mostrar un mensaje de error de red al usuario
+    func showNetworkError() {
+        // Crea una instancia de UIAlertController para mostrar el mensaje de error
+        let alert = UIAlertController(
+            title: "Whoops...",
+            message: "There was an error accessing the iTunes Store." +
+            " Please try again.",
+            preferredStyle: .alert)
+
+        // Agrega un botón de acción "OK" al UIAlertController
+        let action = UIAlertAction(
+            title: "OK", style: .default, handler: nil)
+        alert.addAction(action)
+        
+        // Presenta el UIAlertController para mostrar el mensaje de error al usuario
+        present(alert, animated: true, completion: nil)
+    }
+
+    
 }
